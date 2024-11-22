57c933993399d75d91d349c21627b3893662822b
ap.shimolin <ap.shimolin@gmail.com>
2024-11-22 16:19:48 +1000

    TaskService modified

--- src/main/java/org/example/taskstracker/service/TaskService.java
+++ src/main/java/org/example/taskstracker/service/TaskService.java
@@ -22,101 +22,31 @@ public class TaskService {
     private final UserService userService;
 
     public Flux<TaskModelResponse> findAll() {
-        return taskRepository.findAll().flatMap(task -> {
-            Mono<UserModel> author = userService.findById(task.getAuthorId());
-            Mono<UserModel> assignee = userService.findById(task.getAssigneeId());
-            Mono<List<UserModel>> observers = userService.findAllById(task.getObserverIds()).collectList();
-
-            Mono<Task> taskMono = Mono.zip(author, assignee, observers).flatMap(data -> {
-                task.setAuthor(User.from(data.getT1()));
-                task.setAssignee(User.from(data.getT2()));
-                data.getT3().forEach(um -> {
-                    task.getObservers().add(User.from(um));
-                });
-                return Mono.just(task);
-            });
-            return taskMono.map(TaskModelResponse::from);
-        });
+        return taskRepository.findAll().flatMap(task -> fill(task).map(TaskModelResponse::from));
     }
 
     public Mono<TaskModelResponse> findById(String id) {
-        return taskRepository.findById(id).flatMap(task -> {
-            Mono<UserModel> author = userService.findById(task.getAuthorId());
-            Mono<UserModel> assignee = userService.findById(task.getAssigneeId());
-            Mono<List<UserModel>> observers = userService.findAllById(task.getObserverIds()).collectList();
-
-            Mono<Task> taskMono = Mono.zip(author, assignee, observers).flatMap(data -> {
-                task.setAuthor(User.from(data.getT1()));
-                task.setAssignee(User.from(data.getT2()));
-                data.getT3().forEach(um -> {
-                    task.getObservers().add(User.from(um));
-                });
-                return Mono.just(task);
-            });
-            return taskMono.map(TaskModelResponse::from);
-        });
+        return taskRepository.findById(id).flatMap(task -> fill(task).map(TaskModelResponse::from));
     }
 
     public Mono<TaskModelResponse> create(TaskModelRequest request) {
         Task task = Task.fromRequest(request);
         task.setId(UUID.randomUUID().toString());
         task.setCreatedAt(Instant.now());
-        task.setUpdatedAt(Instant.now());
-//        task.setAuthorId(request.getAuthorId());
-//        task.setAssigneeId(request.getAssigneeId());
-//        task.setObserverIds(request.getObserverIds());
-
-        Mono<UserModel> author = userService.findById(request.getAuthorId());
-        Mono<UserModel> assignee = userService.findById(request.getAssigneeId());
-        Mono<List<UserModel>> observers = userService.findAllById(request.getObserverIds()).collectList();
-
-        Mono<Task> taskMono = Mono.zip(author, assignee, observers).flatMap(data -> {
-            task.setAuthor(User.from(data.getT1()));
-            task.setAssignee(User.from(data.getT2()));
-            data.getT3().forEach(um -> {
-                task.getObservers().add(User.from(um));
-            });
-            return Mono.just(task);
-        });
-
+        Mono<Task> taskMono = fill(task);
         return taskMono.flatMap(t -> taskRepository.save(t).map(TaskModelResponse::from));
     }
 
     public Mono<TaskModelResponse> update(String id, TaskModelRequest request) {
         return taskRepository.findById(id).flatMap(taskForUpdate -> {
-
             if (request.getName() != null) taskForUpdate.setName(request.getName());
             if (request.getDescription() != null) taskForUpdate.setDescription(request.getDescription());
             if (request.getStatus() != null) taskForUpdate.setStatus(request.getStatus());
+            if (request.getAuthorId() != null) taskForUpdate.setAuthorId(request.getAuthorId());
+            if (request.getAssigneeId() != null) taskForUpdate.setAssigneeId(request.getAssigneeId());
+            if (request.getObserverIds() != null) taskForUpdate.setObserverIds(request.getObserverIds());
             taskForUpdate.setUpdatedAt(Instant.now());
-
-            Mono<UserModel> author = request.getAuthorId() != null ?
-                    userService.findById(request.getAuthorId()) : Mono.just(new UserModel());
-            Mono<UserModel> assignee = request.getAssigneeId() != null ?
-                    userService.findById(request.getAssigneeId()) : Mono.just(new UserModel());
-            Mono<List<UserModel>> observers = request.getObserverIds() != null ?
-                    userService.findAllById(request.getObserverIds()).collectList() : Mono.just(new ArrayList<>());
-
-            Mono<Task> taskMono = Mono.zip(author, assignee, observers).flatMap(data -> {
-                if (request.getAuthorId() != null) {
-                    taskForUpdate.setAuthorId(data.getT1().getId());
-                    taskForUpdate.setAuthor(User.from(data.getT1()));
-                }
-                if (request.getAssigneeId() != null) {
-                    taskForUpdate.setAssigneeId(data.getT2().getId());
-                    taskForUpdate.setAssignee(User.from(data.getT2()));
-                }
-                if (request.getObserverIds() != null) {
-                    taskForUpdate.getObserverIds().clear();
-                    taskForUpdate.getObservers().clear();
-                    data.getT3().forEach(um -> {
-                        taskForUpdate.getObserverIds().add(um.getId());
-                        taskForUpdate.getObservers().add(User.from(um));
-                    });
-                }
-                return Mono.just(taskForUpdate);
-            });
-
+            Mono<Task> taskMono = fill(taskForUpdate);
             return taskMono.flatMap(t -> taskRepository.save(t).map(TaskModelResponse::from));
         });
     }
@@ -124,5 +54,24 @@ public class TaskService {
     public Mono<Void> deleteById(String id) {
         return taskRepository.deleteById(id);
     }
+
+    //TODO move to task.class
+    private Mono<Task> fill(Task task) {
+        Mono<UserModel> author = userService.findById(task.getAuthorId());
+        Mono<UserModel> assignee = userService.findById(task.getAssigneeId());
+        Mono<List<UserModel>> observers = userService.findAllById(task.getObserverIds()).collectList();
+
+        return Mono.zip(author, assignee, observers).flatMap(data -> {
+            if (task.getAuthorId() != null) task.setAuthor(User.from(data.getT1()));
+            if (task.getAssigneeId() != null) task.setAssignee(User.from(data.getT2()));
+            if (task.getObserverIds() != null) {
+                data.getT3().forEach(um -> {
+                    task.getObservers().add(User.from(um));
+                });
+            }
+            return Mono.just(task);
+        });
+    }
+
 
 }
